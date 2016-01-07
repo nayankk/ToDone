26e1c9473bd57eb1684ad3de7412fda74d089868
Nayan Kumar K <nayankk@motorola.com>
2016-01-06 19:38:30 -0800

    Add colors

--- app/src/main/java/xc0ffee/todo/TodoCursorAdapter.java
+++ app/src/main/java/xc0ffee/todo/TodoCursorAdapter.java
@@ -2,6 +2,7 @@ package xc0ffee.todo;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,6 +25,20 @@ public class TodoCursorAdapter extends CursorAdapter {
         TextView tv = (TextView) view.findViewById(R.id.text);
         int index = cursor.getColumnIndexOrThrow(TodoDatabase.KEY_TODO);
         tv.setText(cursor.getString(index));
+
+        TextView prioTv = (TextView) view.findViewById(R.id.prio);
+        TodoItem.Priority priority = TodoItem.Priority.values()[
+                cursor.getInt(cursor.getColumnIndexOrThrow(TodoDatabase.KEY_PRIO))];
+        if (priority == TodoItem.Priority.PRIOROTY_HIGH) {
+            prioTv.setText(R.string.high);
+            prioTv.setTextColor(ContextCompat.getColor(context, R.color.color_high));
+        } else if (priority == TodoItem.Priority.PRIOROTY_LOW) {
+            prioTv.setText(R.string.low);
+            prioTv.setTextColor(ContextCompat.getColor(context, R.color.color_low));
+        } else {
+            prioTv.setText(R.string.medium);
+            prioTv.setTextColor(ContextCompat.getColor(context, R.color.color_med));
+        }
     }
 
 }
